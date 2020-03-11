377fae8ee5a9f04559922f20044a83f1f4811b10
Iliasb <barthelemy.junior@gmail.com>
2020-03-11 21:00:25 +0100

    Changed KML structure

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import "ol/ol.css";
 import KML from "ol/format/KML";
+import GeoJSON from "ol/format/GeoJSON";
 import {
   defaults as defaultInteractions,
   DragRotateAndZoom
@@ -9,11 +10,29 @@ import Stamen from "ol/source/Stamen";
 import VectorSource from "ol/source/Vector";
 import { Map, View } from "ol/index";
 
-var doc = document.implementation.createDocument(
+var kmlDoc = document.implementation.createDocument(
   "http://www.opengis.net/kml/2.2",
   "kml",
   null
 );
+
+var documentElement = kmlDoc.createElement("Document");
+kmlDoc.documentElement.appendChild(documentElement);
+
+var documentNameElement = kmlDoc.createElement("name");
+var documentname = kmlDoc.createTextNode("RF Heatmap");
+
+documentNameElement.appendChild(documentname);
+documentElement.appendChild(documentNameElement);
+
+var folderElement = kmlDoc.createElement("Folder");
+documentElement.appendChild(folderElement);
+
+var folderNameElement = kmlDoc.createElement("name");
+var foldername = kmlDoc.createTextNode("Heatmap export");
+
+folderNameElement.appendChild(foldername);
+folderElement.appendChild(folderNameElement);
 
 //https://stackoverflow.com/questions/3191179/generate-xml-document-in-memory-with-javascript
 
@@ -23,17 +42,21 @@ $(document).ready(function() {
 
   var vector = new HeatmapLayer({
     source: new VectorSource({
-      url: "data/kml/2012_Earthquakes_Mag5.kml",
+      url: kmlDoc,
+      //url: "data/kml/2012_Earthquakes_Mag5.kml",
       format: new KML({
         extractStyles: false
       })
     }),
+
     blur: parseInt(blur.value, 10),
     radius: parseInt(radius.value, 10),
     weight: function(feature) {
-      var name = feature.get("name");
-      var magnitude = parseFloat(name.substr(2));
-      return magnitude - 5;
+      //var name = feature.get("name");
+      //var magnitude = parseFloat(name.substr(2));
+      //return magnitude - 5;
+      alert(feature.get("name"));
+      return feature.get("name");
     }
   });
 
@@ -55,22 +78,25 @@ $(document).ready(function() {
   });
 
   $("#addPoint").click(function() {
-    var placemarkElement = doc.createElement("Placemark");
+    var placemarkElement = kmlDoc.createElement("Placemark");
 
-    var nameElement = doc.createElement("name");
-    var name = doc.createTextNode($("#dbm").val());
+    var nameElement = kmlDoc.createElement("name");
+    var name = kmlDoc.createTextNode($("#dbm").val());
 
-    var pointElement = doc.createElement("point");
-    var point = doc.createTextNode($("#lat").val() + "," + $("#long").val());
+    var pointElement = kmlDoc.createElement("point");
+    var point = kmlDoc.createTextNode($("#lat").val() + "," + $("#long").val());
 
-    var descriptionElement = doc.createElement("description");
-    var description = doc.createTextNode($("#remarks").val());
+    var descriptionElement = kmlDoc.createElement("description");
+    var description = kmlDoc.createTextNode($("#remarks").val());
 
-    var weatherElement = doc.createElement("weather");
-    var weather = doc.createTextNode($("#weather").val());
+    var weatherElement = kmlDoc.createElement("weather");
+    var weather = kmlDoc.createTextNode($("#weather").val());
 
-    var buildingElement = doc.createElement("building");
-    var building = doc.createTextNode($("#building").val());
+    var buildingElement = kmlDoc.createElement("building");
+    var building = kmlDoc.createTextNode($("#building").val());
+
+    var datetimeElement = kmlDoc.createElement("datetime");
+    var datetime = kmlDoc.createTextNode($("#datetime").val());
 
     // append nodes to parents
     nameElement.appendChild(name);
@@ -88,12 +114,16 @@ $(document).ready(function() {
     buildingElement.appendChild(building);
     placemarkElement.appendChild(buildingElement);
 
+    datetimeElement.appendChild(datetime);
+    placemarkElement.appendChild(datetimeElement);
+
     // Append to document
-    doc.documentElement.appendChild(placemarkElement);
+    folderElement.appendChild(placemarkElement);
     // Close the modal
     $("#newlocation").modal("hide");
     // Clear the remarks field of the form
     $("#remarks").val("");
+
     // Render the new kml object
     var source = vector.getSource();
     source.refresh();
@@ -101,7 +131,7 @@ $(document).ready(function() {
     ///////////////////////////
 
     var serializer = new XMLSerializer();
-    alert(serializer.serializeToString(doc));
+    alert(serializer.serializeToString(kmlDoc));
   });
 
   map.on("click", function(event) {
